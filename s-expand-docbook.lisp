24ac2232b1e826f96d370af258c60c398e2c717d
Neil T. Dantam <ntd@gatech.edu>
2009-09-21 11:44:47 -0400

    Added some stuff to the docbook macros.

--- s-expand-docbook.lisp
+++ s-expand-docbook.lisp
@@ -51,23 +51,86 @@
 (defun transform-docfun (name description result &rest args)
   `(para (programlisting 
           ,(format nil "~A ~{~A ~} =>  ~A" 
-                   name (mapcar (lambda (arg)
-                                  (if (atom arg) arg (car arg)))
-                                  args)
+                   name (mapcar 
+                         (lambda (arg)
+                           (let ((name
+                                  (if (atom arg)
+                                      arg (car arg))))
+                             (cond 
+                               ((and (symbolp name)
+                                     (string= "&KEY"
+                                              (symbol-name name)))
+                                "&amp;key")
+                               (t name))))
+                         args)
                    result))
          ,@(if (atom description) (list description) description)
-         ,@(let ((details (mapcan (lambda (arg) 
-                                    (when (and (listp arg)
-                                               (cdr arg))
-                                      `((varlistentry (term (varname ,(car arg)))
-                                                      (listitem (para
-                                                                 ,@(cdr arg)))))))
-                                  args)))
+         ,@(let ((details 
+                  (mapcan (lambda (arg) 
+                            (when (and (listp arg)
+                                       (cdr arg))
+                              `((varlistentry (term (varname ,(car arg)))
+                                              (listitem (para
+                                                         ,@(cdr arg)))))))
+                          args)))
                 (when details
                   `((variablelist ,@details))))))
+
+(defun transform-escape-entities (x)
+  (with-output-to-string (s)
+    (loop for c across (string x)
+       do (case c
+            (#\& (write-string "&amp;") s)
+            (#\< (write-string "&lt;") s)
+            (#\> (write-string "&gt;") s)
+            (otherwise (write-char c s))))))
+
+(defun transform-bsd-license (section-level title year holder)
+  `(:sect ,section-level ,title
+          (para
+           "Copyright" ((trademark class copyright)) ,year ,holder)
+          (para "All rights reserved.")
+          (para
+           "Redistribution and use in source and binary forms,
+            with or without modification, are permitted provided that
+            the following conditions are met:")
+
+          (itemizedlist
+           (listitem (para "Redistributions of source code must retain
+                       the above copyright notice, this list of
+                       conditions and the following disclaimer."))
+           (listitem (para "Redistributions in binary form must
+                      reproduce the above copyright notice, this list
+                      of conditions and the following disclaimer in
+                      the documentation and/or other materials
+                      provided with the distribution."))
+           (listitem (para "Neither the name of the Copyright Holders
+                       nor the names of their contributors may be used
+                       to endorse or promote products derived from
+                       this software without specific prior written
+                       permission.")))
+                  
+          (para "THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS
+                 AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED
+                 WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+                 IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+                 A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+                 SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+                 FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+                 EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+                 (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+                 SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+                 PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+                 ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+                 STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+                 OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+                 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+                 DAMAGE.")))
 
 
 
 (defparameter *docbook-transform-alist*
   `((:sect ,#'transform-sect)
-    (:docfun ,#'transform-docfun)))
+    (:docfun ,#'transform-docfun)
+    (:escent ,#'transform-escape-entities)
+    (:bsd-license ,#'transform-bsd-license)))
